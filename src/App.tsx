2d085244f7c226c77ec3261a78bd97c6494ae6a9
Nihar Hegde <niharhegde163@gmail.com>
2023-07-19 19:49:50 +0530

    expences list

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,57 @@ import Cart from "./components/Cart";
 import Navbar from "./components/Navbar";
 import ExpandableText from "./components/ExpandableText";
 import Form from "./components/Form";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
+import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [expenses, setExpenses] = useState([
+    {
+      id: 1,
+      description: "aaa",
+      amount: 10,
+      category: "Utilities",
+    },
+    {
+      id: 2,
+      description: "bbb",
+      amount: 20,
+      category: "Groceries",
+    },
+    {
+      id: 3,
+      description: "ccc",
+      amount: 40,
+      category: "Utilities",
+    },
+    {
+      id: 4,
+      description: "ddd",
+      amount: 50,
+      category: "IceCream",
+    },
+    {
+      id: 5,
+      description: "eee",
+      amount: 100,
+      category: "medical",
+    },
+  ]);
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((e) => e.category == selectedCategory)
+    : expenses;
   return (
     <>
-      <Form></Form>
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        ></ExpenseFilter>
+      </div>
+      <ExpenseList
+        expenses={visibleExpenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      ></ExpenseList>
     </>
   );
 }
